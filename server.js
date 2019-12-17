39c8fc2ff396de169f2e6a07410b5b390a4941cf
Idris <idrist2013@gmail.com>
2019-12-17 14:19:04 +0000

    homepage

---
+++ server.js
@@ -0,0 +1,9 @@
+function addnum(a, b) {
+  if (a === 100 || b === 100) {
+    console.log("its 100");
+  } else {
+    console.log("none");
+  }
+}
+
+addnum(10, 20);
---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+var inStock = true;
+var shipping = false;
+
+var elstock = document.getElementById("stock");
+elstock.className = inStock;
+
+var elship = document.getElementById("shipping");
+elship.className = shipping;
