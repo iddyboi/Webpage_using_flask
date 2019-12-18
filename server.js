7395d4d0ce800438959b3997bedbcf9a57f1cdb2
Idris <idrist2013@gmail.com>
2019-12-18 21:01:32 +0000

    remove twilio

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
 const Twilio = require("twilio");
-
-const client = new Twilio(
-  "AC45aafa0f5f48f149a4f0371fd06d048b",
-  "4a595e06b7114921b3240d86796684d0"
-);
-
-client.messages
-  .list()
-  .then(messages => console.log(`most recent message is ${messages[0].body}`))
-  .catch(err => console.error(err));
--- temp.py
+++ temp.py
@@ -6,18 +6,3 @@
 # soup = BeautifulSoup(req.text, 'html.parser')
 # results = soup.find_all('p')
 # print(results[4].text)
-import requests
-
-url = "https://ronreiter-meme-generator.p.rapidapi.com/meme"
-
-querystring = {"font": "Impact", "font_size": "50",
-               "meme": "Condescending-Wonka", "top": "Top text", "bottom": "Bottom text"}
-
-headers = {
-    'x-rapidapi-host': "ronreiter-meme-generator.p.rapidapi.com",
-    'x-rapidapi-key': "SIGN-UP-FOR-KEY"
-}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
